857d2f149c0c25869a70b65815878815e8efd962
amit singh <amitsingh211196@gmail.com>
2022-01-25 11:07:26 +0530

    Added Displaying Products & Rating Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="my-3">
         <Container>
           <h1>Ecommerce App</h1>
+          <HomeScreen />
         </Container>
       </main>
 
